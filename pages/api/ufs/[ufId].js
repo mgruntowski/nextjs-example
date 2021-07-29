26db87b56deb7b1c7452f6133a6ed1c62aaaf2b4
Mateus Gruntowski Mendes <mateus.mendes@escolaconquer.com.br>
2021-07-29 16:09:30 -0300

    teste ISR

---
+++ pages/api/ufs/[ufId].js
@@ -0,0 +1,17 @@
+import nextConnect from 'next-connect'
+import dbMiddleware from '../../../middlewares/database'
+import { ObjectId } from 'mongodb'
+
+const handler = nextConnect()
+
+handler.use(dbMiddleware)
+
+handler.get(async (req, res) => {
+    const { ufId } = req.query
+
+    const doc = await req.db.collection('oaes').findOne({ _id: ObjectId(ufId) })
+
+    res.json(doc)
+})
+
+export default handler
