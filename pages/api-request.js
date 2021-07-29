8346c43b690a08e14ba4de5df77ba63f2f396361
Mateus Gruntowski Mendes <mateus.mendes@escolaconquer.com.br>
2021-07-29 17:55:04 -0300

    adição image

--- pages/api-request.js
+++ pages/api-request.js
@@ -1,5 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image'
+
 export default function ApiRequest(props) {
     return (<>
+        <Image src="/img/scooby.jpg" alt="scooby" width="512" height="768"/>
+        <img src="/img/scooby.jpg" alt="scooby" width="512" height="768"/>
         {props.data.map((d, index) =>
             <a key={index} href={`/api-request/${d._id}`} target='_blank' rel="noreferrer">
                 <p>{JSON.stringify(d)}</p>
