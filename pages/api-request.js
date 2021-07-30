a09c017ed25b372393df5a3db49921f2cde4ff75
Mateus Gruntowski Mendes <mateus.mendes@escolaconquer.com.br>
2021-07-30 10:52:05 -0300

    remoção temporária image

--- pages/api-request.js
+++ pages/api-request.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 export default function ApiRequest(props) {
     return (<>
-        <Image src="/img/scooby.jpg" alt="scooby" width="512" height="768"/>
-        <img src="/img/scooby.jpg" alt="scooby" width="512" height="768"/>
+        {/* <Image src="/img/scooby.jpg" alt="scooby" width="512" height="768"/>
+        <img src="/img/scooby.jpg" alt="scooby" width="512" height="768"/> */}
         {props.data.map((d, index) =>
             <a key={index} href={`/api-request/${d._id}`} target='_blank' rel="noreferrer">
                 <p>{JSON.stringify(d)}</p>
